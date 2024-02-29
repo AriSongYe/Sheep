b880cb5590c6cde90d5500ee0bce336a6ff94ceb
AriSongYe <kow14671@gmail.com>
2024-03-01 07:55:26 +0900

    anmation complete

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ class App {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
+
         this.hills = [new Hill('#fd6674', 0.2, 12),
         new Hill('#ff59c2', 0.5, 8),
         new Hill('#ff4674', 0.7, 10)];
         
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize()
-        window.requestAnimationFrame(this.animate.bind(this));
+        
+        requestAnimationFrame(this.animate.bind(this));
 
     }
 
@@ -29,11 +31,13 @@ class App {
     }
 
     animate(t) {
-        window.requestAnimationFrame(this.animate.bind(this));
+        requestAnimationFrame(this.animate.bind(this));
+
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
 
+        let dots;
         for(let i = 0; i < this.hills.length; i++) {
-            this.hills[i].draw(this.ctx);
+            dots = this.hills[i].draw(this.ctx);
         }
     }
 }
