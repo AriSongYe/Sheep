5d87cea47e2d2ae7047381cf1fc2e731761dfb2f
AriSongYe <kow14671@gmail.com>
2024-03-04 18:36:38 +0900

    Sheeps coordi

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import { Hill } from './hill.js';
 import { SheepController } from './sheep-controller.js';
+import { Sun } from './sun.js';
 
 class App {
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
+
+        this.sun = new Sun();
 
         this.hills = [new Hill('#fd6674', 0.2, 12),
         new Hill('#ff59c2', 0.5, 8),
@@ -22,7 +25,7 @@ class App {
     resize() {
         this.stageWidth = document.body.clientWidth;
         this.stageHeight = document.body.clientHeight;
-
+        this.sun.resize(this.stageWidth, this.stageHeight);
         this.canvas.width = this.stageWidth * 2;
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2, 2);
@@ -40,12 +43,12 @@ class App {
         requestAnimationFrame(this.animate.bind(this));
 
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-
+        
+        this.sun.draw(this.ctx, t);
         let dots;
         for(let i = 0; i < this.hills.length; i++) {
             dots = this.hills[i].draw(this.ctx);
         }
-
         this.sheepController.draw(this.ctx, t, dots);
     }
 }
